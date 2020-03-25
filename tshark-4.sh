50506c102e14bc1b8f9ebe170d452a4d82f37f15
Joshua <newheart0561@gmail.com>
2020-03-26 01:09:22 +0800

    modify tshark-4.sh

--- tshark-4.sh
+++ tshark-4.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-tshark -r hw1-tshark.pcapng -qz "io,stat,1,SUM(frame.len)frame.len"|sed "s/Dur/Dur /g"|awk '{printf"%s %s\n",$2,$6}'|sed "s/[^0-9 ]//g"|sort -k2 -nr|awk '{printf"%s\n",$1}'|head -n $1
-#|sort -nr|head -n 80
-#sources=$(tshark -r hw1-tshark.pcapng -o "gui.column.format:Time,%t,Length,%L")
-#echo "$sources"|sed "s/.[0-9]\{9\}//g"|awk '{arr[$1]+=int($2)}'
-# {for(i in arr)printf"%s %s\n",$i,arr[i]}'>output
-#|sort -k2 -rn >output
-#|awk '{printf"%s\n",$1}'|head -n $1
+tshark -r hw1-tshark.pcapng -qz "io,stat,1,SUM(frame.len)frame.len"|sed "s/Dur/Dur /g"|awk '{printf"%s %s\n",$2,$6}'|sed "s/[^0-9 ]//g"|sort -k2 -nr|awk '{printf"%s\n",$1}'|uniq|sed -e '$d'|head -n $1
