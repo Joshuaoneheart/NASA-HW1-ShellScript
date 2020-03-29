ac490b8dc040fe9b39e46c9116def9b8f17b2fbc
Joshua <newheart0561@gmail.com>
2020-03-29 17:49:31 +0800

    modified version of tshark-2.sh

--- tshark-2.sh
+++ tshark-2.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 file="hw1-tshark.pcapng"
 MY_TEMP=$(mktemp)
-sorted=$(tshark -r $file -e frame.len -T fields|sed "s/ /\n/g"|sort -run|head -n $1)
 output=""
 i=0
-for item in $sorted
+sources=$(tshark -r $file  -o "gui.column.format:Time,%Yt,Source,%s,Destination,%d,Length,%L"|sort -k 6nr -k 2)
+echo "$sources" > $MY_TEMP
+i=0
+while read line
 do
-	sources=$(tshark -r $file  -o "gui.column.format:Time,%Yt,Source,%s,Destination,%d" -Y "frame.len==$item")
-	echo "$sources" > $MY_TEMP
-	while read line
-	do
-		if [[ $i -ge $1 ]];then
-			break;
-		fi
-		i=$((i + 1))
-		source="$(echo "$line"|sed "s/.[0-9]\{9\}//g"|awk '{printf"%s %s,%s,%s",$1,$2,$3,$5}')"
-		echo "$source"
-	done < $MY_TEMP
-	if [[ $i -ge $1 ]];then
-		break;
+	if [[ $i -eq $(($1)) ]]; then
+	break;
 	fi
-done
+	i=$(($i + 1))
+	source="$(echo "$line"|sed "s/.[0-9]\{9\}//g"|awk '{printf"%s %s,%s,%s",$1,$2,$3,$5}')"
+	echo "$source"
+done < $MY_TEMP
 rm -rf $MY_TEMP
