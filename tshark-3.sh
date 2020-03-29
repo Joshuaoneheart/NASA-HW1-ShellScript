91c40c08741cdd46f2d446d8e0f2b3a7f0351a2d
Joshua <newheart0561@gmail.com>
2020-03-29 17:25:40 +0800

    new version of tshark-3.sh

--- tshark-3.sh
+++ tshark-3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sources="$(tshark -r hw1-tshark.pcapng -Y "dns" -T fields -e ip.src|sed "s/,[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*//g"|sort)"
-sources="$(echo "$sources"|uniq -c|sort -nr)"
+sources="$(echo "$sources"|uniq -c|sort -k2|sort -nr -k1)"
+echo "$sources"
 sources=$(echo "$sources"|head -n $1|sed "s/^[ ]*//g"|sed "s/ /,/g")
 for line in $(echo "$sources")
 do
